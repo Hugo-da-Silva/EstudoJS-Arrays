c51326f37c8be1072dbe3a12a4f01528bc2e3c0c
Hugo da Silva <hdasilva97@gmail.com>
2023-01-03 15:54:46 -0300

    Arrays (vetores)
    Exercício com base no estudo no estudo de arrays em JavaScript

---
+++ ambiente.js
@@ -0,0 +1,7 @@
+let num = [9, 5, 4, 2, 0]
+//num.sort()
+console.log(num)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O vetor tem ${num.length} posições`)
+let pos =  num.indexOf(4)
+console.log(`A posição do valor 4 é ${pos}`)
---
+++ vetornatela.js
@@ -0,0 +1,4 @@
+let valores = [10 ,9 ,8 ,7 ,6 ,5 ,4 ,3 ,2 ,1]
+for(let pos in valores){
+    console.log(`A posição do ${pos} tem o valor de ${valores[pos]}`)
+}
