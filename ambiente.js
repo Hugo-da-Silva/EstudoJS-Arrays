ced55e6d60851afb96d8cba5c5bccdead19873a4
Hugo da Silva <hdasilva97@gmail.com>
2023-01-03 16:04:44 -0300

    Atualizando o arquivo
    Apenas atualizando o arquivo para os comentarios feitos

--- ambiente.js
+++ ambiente.js
@@ -1,7 +1,7 @@
 let num = [9, 5, 4, 2, 0]
-//num.sort()
-console.log(num)
-console.log(`O primeiro valor do vetor é ${num[0]}`)
-console.log(`O vetor tem ${num.length} posições`)
-let pos =  num.indexOf(4)
-console.log(`A posição do valor 4 é ${pos}`)
+num.sort() // usamos para colocar em ordem os valores dentro do vetor
+console.log(num) // usamos para mostrar a variavel criada, no caso "num"
+console.log(`O primeiro valor do vetor é ${num[0]}`) // usamos para mostrar junto com place holder e colchetes o primeiro valor do vetor
+console.log(`O vetor tem ${num.length} posições`) // usamos para mostrar quantas posições o vetor tem
+let pos =  num.indexOf(4) // usamos para mostrar a posição do valor 4
+console.log(`A posição do valor 4 é ${pos}`) // usamos para o console nos dizer qual a posição desejada de acordo com a varaivel e o indexOf acima
--- vetornatela.js
+++ vetornatela.js
@@ -1,4 +1,4 @@
 let valores = [10 ,9 ,8 ,7 ,6 ,5 ,4 ,3 ,2 ,1]
-for(let pos in valores){
+for(let pos in valores){ // para cada posição dentro de valores...  
     console.log(`A posição do ${pos} tem o valor de ${valores[pos]}`)
 }
